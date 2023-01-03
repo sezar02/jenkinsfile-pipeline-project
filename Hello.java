6f8a3f40a4018513659e9a26abc0f530acaebac4
EC2 Default User <ec2-user@ip-172-31-85-171.ec2.internal>
2023-01-03 13:12:29 +0000

    change12

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java sezai selamlar");
+        System.out.println("Hello from Java sezai selamlar başardık");
     }
 }
